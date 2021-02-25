9fad9ede5472ac5b74fa7248d2a1669bfe68deb8
robbie <robbielcampbell@hotmail.com>
2021-02-25 18:21:41 +0000

    :shipit: tutorial complete :shipit:

---
+++ drestapi/app/views.py
@@ -0,0 +1,15 @@
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class PostView(generics.RetrieveAPIView):
+    # permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
